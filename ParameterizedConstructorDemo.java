0a9e36dc859ae3174ea921964b1ade5812e2527f
manjusripenumala <143867107+manjusripenumala@users.noreply.github.com>
2023-09-23 12:49:39 +0530

    Create ParameterizedConstructorDemo.java

---
+++ ParameterizedConstructorDemo.java
@@ -0,0 +1,14 @@
+ import java.io.*;
+ import java.util.*;
+ class Rectangle{
+     Rectangle(){}
+     Rectangle(int a,int b){
+         int a = l*b;
+     }
+ }
+ public class ParameterizedConstructorDemo{ 
+     public static void main(String args[]){ 
+         Rectangle r=new Rectangle(5,8); 
+         System.out.println("Area of rectangle is " + r.a);
+        }
+    } 
